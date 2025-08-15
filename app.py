16d792a728ea7dedfb8a143c83191f4340e04ae7
shivaavvari <aisshiva766@gmail.com>
2025-08-15 12:23:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def index():
 
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True) # running the app
+    app.run(host='0.0.0.0', port=8001, debug=True)
+	#app.run(debug=True) # running the app
